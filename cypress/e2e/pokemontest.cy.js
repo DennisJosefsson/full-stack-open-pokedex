1237f82b6d6b345f55cc2e27c6d7db166916e299
Dennis Josefsson <dennis.josefsson@gmail.com>
2023-05-08 14:40:11 +0200

    Added second E2E test

--- cypress/e2e/pokemontest.cy.js
+++ cypress/e2e/pokemontest.cy.js
@@ -6,4 +6,10 @@ describe('Pokedex', function () {
       'Pokémon and Pokémon character names are trademarks of Nintendo.'
     )
   })
+  it('Individual Pokemon page can be opened', function () {
+    cy.visit('http://localhost:5000')
+    cy.contains('metapod').click()
+    cy.contains('Previous')
+    cy.contains('shed skin')
+  })
 })
