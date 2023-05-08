0434a3c44fe72a15d9410185782ab8377234ddd7
Dennis Josefsson <dennis.josefsson@gmail.com>
2023-05-08 19:40:13 +0200

    Updated health check script

--- health_check.sh
+++ health_check.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-echo "Hello from shell script"
+URL="https://bitter-wood-9241.fly.dev/health"
 
-exit 0
+response=$(curl -sb -H "Accept: application/text" $URL)
+
+if [[ "$response" -ne "ok" ]] ; then
+  exit 1
+else
+  echo "Pokemon site healthy"
+fi
